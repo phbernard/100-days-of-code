608fb597e8c2487aeec461eed5e35bf30cdfdacc
Philippe Bernard <philippe.bernard@gmail.com>
2021-11-19 08:22:21 +0100

    Twitter banner update proof of concept

--- update-twitter-banner.ts
+++ update-twitter-banner.ts
@@ -1,1 +1,30 @@
-console.log("Hello");
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { TwitterClient } from 'twitter-api-client';
+import { domainToUnicode } from 'url';
+import fs from 'fs/promises';
+
+const doId = async() => {
+  if (!process.env.TWITTER_API_KEY || !process.env.TWITTER_API_SECRET) {
+    console.log("ERROR: Twitter credentials are not configured");
+    return;
+  }
+
+  const twitterClient = new TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY,
+    apiSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  const banner = await fs.readFile('my-image.png', {encoding: 'base64'});
+
+  twitterClient.accountsAndUsers.accountUpdateProfileBanner({
+    banner
+  });
+}
+
+(() => {
+  doId();
+})();
