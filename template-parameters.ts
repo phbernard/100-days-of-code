a5b7dcc20b534d1e602c3541ef2d0266b8fcf2a0
Philippe Bernard <philippe.bernard@gmail.com>
2021-11-25 11:39:46 +0100

    Regenerate the banner every 2 hours

--- template-parameters.ts
+++ template-parameters.ts
@@ -3,6 +3,12 @@ import { TwitterClient } from 'twitter-api-client';
 import fs from 'fs';
 import axios from 'axios';
 
+const addHours = (date: Date, hours: number): Date => {
+  const future = new Date();
+  future.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+  return future;
+}
+
 export const bannerTemplateParameters = async (twitterClient: TwitterClient): Promise<ParamValues> => {
   // User
   const user  = await twitterClient.accountsAndUsers.usersShow({ screen_name: '@ph_bernard' });
@@ -31,10 +37,15 @@ export const bannerTemplateParameters = async (twitterClient: TwitterClient): Pr
   const gitHubResponse = await axios.get('https://api.github.com/repos/resocio/resoc');
   const resocStars = gitHubResponse.data.stargazers_count;
 
+  // Next generation
+  const next = addHours(new Date(), 2);
+  const nextGenerationDate = `${next.getUTCDate()}/${next.getUTCMonth() + 1}/${next.getUTCFullYear()} at ${next.getUTCHours()}:${next.getUTCMinutes()} UTC`;
+
   return {
     followerCount,
     day: currentDay.toString(),
     activity: daysStatus.map(status => ({ status: status ? 'completed' : 'missed' })),
-    resocStars
+    resocStars,
+    nextGenerationDate
   }
 };
